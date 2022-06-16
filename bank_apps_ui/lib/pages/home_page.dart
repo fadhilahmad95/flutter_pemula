2a2123b17c7c795fd9d9999ae5e0db1ee105ef92
dhilco_ <fadhilahmad95@gmail.com>
2022-06-16 12:43:51 +0800

    first

---
+++ bank_apps_ui/lib/pages/home_page.dart
@@ -0,0 +1,81 @@
+import 'package:bank_apps_ui/thema.dart';
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: blueColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.only(top: 30),
+                child: const Center(
+                  child: Text(
+                    "My Logo",
+                    style: TextStyle(
+                      fontSize: 28,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 30),
+                child: Image.asset(
+                  "assets/img/ilustration.png",
+                  width: 300,
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                padding: const EdgeInsets.all(30),
+                width: double.infinity,
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    const Text(
+                      "Banking Apps",
+                      style: TextStyle(
+                        fontSize: 32,
+                        color: blueColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    const Text(
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry been the industry's ",
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 30),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 40,
+                          vertical: 12,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        primary: blueColor,
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        "Sign Up",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ bank_apps_ui/lib/thema.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color blueColor = Color(0xff414FAA);
+const Color whiteColor = Color(0xffFFFFFF);
+const Color bluelightColor = Color(0xff6F8ACE);
+
