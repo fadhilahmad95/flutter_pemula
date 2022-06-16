cb201e0d11d394ca3b9980ba8ce983d296fec327
dhilco_ <fadhilahmad95@gmail.com>
2022-06-16 16:08:10 +0800

    home-screen

--- bank_apps_ui/lib/main.dart
+++ bank_apps_ui/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bank_apps_ui/pages/home_page.dart';
+import 'package:bank_apps_ui/pages/sign_up_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SignUpPage(),
+      home: HomePage(),
     );
   }
 }
--- bank_apps_ui/lib/pages/home_page.dart
+++ bank_apps_ui/lib/pages/home_page.dart
@@ -1,80 +1,262 @@
 import 'package:bank_apps_ui/thema.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
-class SignUpPage extends StatelessWidget {
-  const SignUpPage({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: blueColor,
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.only(top: 30),
-                child: const Center(
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: blueColor,
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(12),
+                        bottomRight: Radius.circular(12)),
+                  ),
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: const [
+                            Icon(
+                              Icons.menu,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                            Icon(
+                              Icons.notifications_outlined,
+                              color: Colors.white,
+                              size: 30,
+                            )
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        "Welcome Back",
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      const Text(
+                        "Nick Julian",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        "Current Balance",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 6),
+                      const Text(
+                        "\$ 1,245",
+                        style: TextStyle(
+                          fontSize: 46,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 6),
+                      Container(
+                        width: 280,
+                        child: Text(
+                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy ",
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: const [
+                            IconsMenu(
+                              image: "assets/img/icon_menu_satu.png",
+                              width: 18,
+                              name: "Pay",
+                            ),
+                            IconsMenu(
+                              image: "assets/img/icon_menu_dua.png",
+                              width: 18,
+                              name: "Receive",
+                            ),
+                            IconsMenu(
+                              image: "assets/img/icon_menu_tiga.png",
+                              width: 18,
+                              name: "Bills",
+                            ),
+                            IconsMenu(
+                              image: "assets/img/icon_menu_empat.png",
+                              width: 24,
+                              name: "Transaction",
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 20),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: const [
+                            IconsMenu(
+                              image: "assets/img/icon_menu_lima.png",
+                              width: 26,
+                              name: "Loans",
+                            ),
+                            IconsMenu(
+                              image: "assets/img/icon_menu_enam.png",
+                              width: 43,
+                              name: "Credit\nCard",
+                            ),
+                            IconsMenu(
+                              image: "assets/img/icon_menu_tujuh.png",
+                              width: 40,
+                              name: "Mutual\nFunds",
+                            ),
+                            IconsMenu(
+                              image: "assets/img/icon_menu_delapan.png",
+                              width: 30,
+                              name: "Fixed\nDeposits",
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
                   child: Text(
-                    "My Logo",
+                    "Recent Transcation",
                     style: TextStyle(
-                      fontSize: 28,
-                      color: Colors.white,
+                      fontSize: 24,
+                      color: blueColor,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.only(top: 30),
-                child: Image.asset(
-                  "assets/img/ilustration.png",
-                  width: 300,
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 20),
-                padding: const EdgeInsets.all(30),
-                width: double.infinity,
-                color: Colors.white,
-                child: Column(
-                  children: [
-                    const Text(
-                      "Banking Apps",
-                      style: TextStyle(
-                        fontSize: 32,
-                        color: blueColor,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 15),
-                    const Text(
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry been the industry's ",
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 30),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 40,
-                          vertical: 12,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        primary: blueColor,
-                      ),
-                      onPressed: () {},
-                      child: const Text(
-                        "Sign Up",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
+                Container(
+                  margin: EdgeInsets.only(top: 20, bottom: 20),
+                  child: Column(
+                    children: [
+                      RecentCard(),
+                      RecentCard(),
+                      RecentCard(),
+                      RecentCard(),
+                      RecentCard(),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class RecentCard extends StatelessWidget {
+  const RecentCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+        left: 35,
+        right: 35,
+        top: 20,
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: 60,
+            height: 60,
+            color: bluelightColor,
+          ),
+          SizedBox(width: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [Text("Cash Withdrawal"), Text("10.30 23 Aug")],
+          ),
+          Spacer(),
+          Text("\$ 304")
+        ],
+      ),
+    );
+  }
+}
+
+class IconsMenu extends StatelessWidget {
+  final String image;
+  final double width;
+  final String name;
+  const IconsMenu({
+    Key? key,
+    required this.image,
+    required this.width,
+    required this.name,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              primary: blueColor,
+              padding: EdgeInsets.all(12),
+            ),
+            onPressed: () {},
+            child: Image.asset(
+              image,
+              width: width,
+            ),
+          ),
+          SizedBox(height: 6),
+          Text(
+            name,
+            textAlign: TextAlign.center,
+          ),
+        ],
       ),
     );
   }
